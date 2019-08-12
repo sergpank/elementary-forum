313f26bc3a0ca58ced2a04e08623a1536d16e0a7
alexzhravlov <alexzhuravlov13@gmail.com>
2019-08-12 20:00:48 +0300

    ok

---
+++ src/main/java/org/elementary/forum/servlets/CreatePostServlet.java
@@ -0,0 +1,19 @@
+package org.elementary.forum.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CreatePostServlet extends HttpServlet
+{
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+  {
+    RequestDispatcher view = req.getRequestDispatcher("postcreate.jsp");
+    view.forward(req,resp);
+
+  }
+}
