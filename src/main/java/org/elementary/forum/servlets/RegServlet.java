d6aed83c5abffc492f370a0beb093c1362bdd096
Konstantin <kirpichk@gmail.com>
2019-08-12 21:37:23 +0300

    finished

--- src/main/java/org/elementary/forum/servlets/RegServlet.java
+++ src/main/java/org/elementary/forum/servlets/RegServlet.java
@@ -1,5 +1,8 @@
 package org.elementary.forum.servlets;
 
+import org.elementary.forum.dao.UserDao;
+import org.elementary.forum.entites.User;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 public class RegServlet extends HttpServlet {
     @Override
@@ -15,6 +19,12 @@ public class RegServlet extends HttpServlet {
     {
         String name = req.getParameter("userName");
         String password = req.getParameter("password");
+        User user = new User();
+        user.setLogin(name);
+        user.setPassword(password);
+        user.setRegistrationDate(new Date());
+        UserDao userDao = new UserDao();
+        userDao.save(user);
         System.out.println(name + "  " + password);
     }
 }
