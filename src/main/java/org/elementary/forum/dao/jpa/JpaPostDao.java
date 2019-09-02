115d04d66d4c522d00a5a3af259f69fa48c06337
Dmitry501 <fedorov.dm501@gmail.com>
2019-09-02 20:11:55 +0300

    upd

--- src/main/java/org/elementary/forum/dao/jpa/JpaPostDao.java
+++ src/main/java/org/elementary/forum/dao/jpa/JpaPostDao.java
@@ -1,5 +1,44 @@
 package org.elementary.forum.dao.jpa;
 
+import org.elementary.forum.entities.Post;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class JpaPostDao
 {
+
+  public Post getById(long id)
+  {
+    EntityManager em = JpaUtil.getEntityManager();
+
+    Post post = em.find(Post.class, id);
+
+    em.close();
+
+    return post;
+  }
+
+  public void save(Post t)
+  {
+    EntityManager em = JpaUtil.getEntityManager();
+
+    em.getTransaction().begin();
+    em.persist(t);
+    em.getTransaction().commit();
+
+    em.close();
+  }
+
+  public List<Post> loadAll(long topicId)
+  {
+    EntityManager em = JpaUtil.getEntityManager();
+
+    List<Post> posts = em
+        .createQuery("from Post p where p.topic.id = :topic_id", Post.class)
+        .setParameter("topic_id", topicId)
+        .getResultList();
+
+    return posts;
+  }
 }
--- src/test/java/JpaPostDaoTest.java
+++ src/test/java/JpaPostDaoTest.java
@@ -1,3 +1,43 @@
+import org.elementary.forum.dao.jpa.JpaPostDao;
+import org.elementary.forum.dao.jpa.JpaTopicDao;
+import org.elementary.forum.entities.Post;
+import org.elementary.forum.entities.Topic;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+
 public class JpaPostDaoTest
 {
+  @Test
+  public void testSaveAndGet()
+  {
+    Post expected = new Post();
+    expected.setAuthorId(0);
+    expected.setDateCreated(new Date());
+    expected.setTopic(null);
+    expected.setText("test text");
+
+    JpaPostDao dao = new JpaPostDao();
+
+    dao.save(expected);
+    System.out.println("Post saved: " + expected);
+
+    Post actual = dao.getById(expected.getId());
+
+    System.out.println("Post found: " + actual);
+  }
+
+  @Test
+  public void testLoadAll()
+  {
+    JpaPostDao dao = new JpaPostDao();
+
+    List<Post> posts = dao.loadAll(1L);
+
+    for (Post p : posts)
+    {
+      System.out.println(p);
+    }
+  }
 }
